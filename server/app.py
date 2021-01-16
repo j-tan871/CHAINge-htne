89bd5b9ec66044929cb8b2aab28e9d60632bc2c2
Bryant Hou <gbhou5@gmail.com>
2021-01-15 19:58:49 -0500

    Added initial endpoint file

---
+++ server/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/createUser', methods = ['POST'])
+def create_user():
+    data = request.json
+    username = data['username']
+    password = data['password']
+
+@app.route('/getBalance', methods = ['GET'])
+def get_balance():
+    data = request.json
+    username = data['username']
+
+if __name__ == "__main__":
+    app.debug = True
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
