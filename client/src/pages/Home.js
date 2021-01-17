548edd5218fa7bc07e02f174c584acfe4a16e1bd
j-tan871 <60245545+j-tan871@users.noreply.github.com>
2021-01-16 21:25:54 -0500

    reformat

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,13 +9,14 @@ const screenHeight = window.innerHeight;
 const Home = () => {
   return (
     <React.Fragment>
-      <div style={{...styles.window, alignItems: 'center', marginTop: 100, backgroundImage: `url(${image})`, backgroundRepeat:"no-repeat", backgroundPosition: "center", backgroundSize: "absolute" }}>
+      <div style={{ height:100 }} className="d-flex justify-content-center">
         <h1>CHAINge: be the chainge you wish to see</h1>
       </div>
-      <div style={styles.window}>
+      <div style={{...styles.window, alignItems: 'center', marginTop: 0, backgroundImage: `url(${image})`, backgroundRepeat:"no-repeat", backgroundPosition: "center", backgroundSize: "absolute" }}></div>
+      <div style={ styles.window }>
         <Container>
           <Row>
-            <h1>About Us</h1>
+            <h1 className="mt-4">About Us</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </Row>
         </Container>
@@ -26,7 +27,7 @@ const Home = () => {
 
 const styles = {
   window: {
-    height: screenHeight,
+    height: screenHeight - 240,
     display: 'flex',
     flexDirection: 'column'
   },
